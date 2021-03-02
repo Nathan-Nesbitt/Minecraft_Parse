4ca26821d4f94b5485d6882f22cfd2a6659c6c6f
Nathan Nesbitt <Nathan-Nesbitt@users.noreply.github.com>
2021-03-02 10:56:01 -0800

    Added errors when file passed is not formatted properly

--- minecraft_parse.js
+++ minecraft_parse.js
@@ -25,14 +25,24 @@ class MinecraftParse {
         }
 
         this.file = file;
+        if(!this.file)
+            throw new Error("No file was passed to MinecraftParse");
+        // Defaults all the values //
         this.title = null;
         this.description = {};
         this.overview = {};
         this.graph = null;
         this.graph_name = null;
-        this.lesson = null;
         this.steps = {};
+        // Parse the file //
         this.parse_file();
+        // Check if required values got set //
+        if(!this.title)
+            throw new Error("The file specified is missing a title");
+        if(!this.description)
+            throw new Error("The file specified is missing a description");
+        if(!this.steps.text || !this.steps.code) 
+            throw new Error("The file specified is missing parts of the lesson");
     }
 
     /**
@@ -44,14 +54,19 @@ class MinecraftParse {
         this.description = this.file.match(RegExp(this.regex.description))[1]
         
         var overview = this.file.split(RegExp(this.regex.overview))[2]
+        if(!overview)
+            throw new Error("The file specified doesn't have a proper overview section");
         this.overview.title = overview.match(RegExp(this.regex.overview_values))[1]
         this.overview.data = overview.match(RegExp(this.regex.overview_values))[2]
         
         this.graph = this.file.match(RegExp(this.regex.graph))[2]
         this.graph_name = this.file.match(RegExp(this.regex.graph))[1]
         
-        this.lesson = this.file.split(RegExp(this.regex.lesson))[2]
-        var steps = this.lesson.split(RegExp(this.regex.step))
+        var lesson = this.file.split(RegExp(this.regex.lesson))[2]
+        if(!lesson)
+            throw new Error("The file specified doesn't have a proper lesson section");
+        
+        var steps = lesson.split(RegExp(this.regex.step))
         for(var i = 1; i < steps.length; i+=4) {
             this.steps[steps[i]] = {
                 "text": steps[i+1],
